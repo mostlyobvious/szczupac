0274a353dcc0b018a120edfce03595f55469ec8b
Paweł Pacana <pawel.pacana@gmail.com>
2023-07-20 16:51:12 +0200

    Refactor

--- lib/szczupac.rb
+++ lib/szczupac.rb
@@ -36,20 +36,7 @@ module Szczupac
   end
 
   def call(**named_lanes)
-    named_lanes
-      .flat_map do |name, values|
-        first_from_rest =
-          named_lanes
-            .reject { |n, _| n == name }
-            .map { |n, v| { n => v.first } }
-            .reduce(&:merge)
-        values
-          .map { |v| { name => v } }
-          .product([first_from_rest])
-          .map { |v| v.reduce(&:merge) }
-          .map { |v| v.sort_by { |k, _| named_lanes.keys.index(k) }.to_h }
-      end
-      .uniq
+    generate(*named_lanes.map { |name, values| axis(name, values) })
   end
   alias [] call
 end
